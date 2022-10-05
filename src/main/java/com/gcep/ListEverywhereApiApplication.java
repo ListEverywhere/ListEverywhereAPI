63728acc1d30b09e261b9e11208ec0e4e89be2f8
gcep650 <gabrielcnew2@gmail.com>
2022-10-05 11:39:28 -0700

    initial app commit

---
+++ src/main/java/com/gcep/ListEverywhereApiApplication.java
@@ -0,0 +1,13 @@
+package com.gcep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListEverywhereApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListEverywhereApiApplication.class, args);
+	}
+
+}
