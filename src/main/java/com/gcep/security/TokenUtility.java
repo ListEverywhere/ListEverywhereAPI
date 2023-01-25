f021d3a820fd093dd59dd9414f034de8dc5b318a
gcep650 <gabrielcnew2@gmail.com>
2023-01-25 12:25:14 -0700

    Users API auth - commit 4
    - Removed and replaced deprecated methods in TokenUtility

--- src/main/java/com/gcep/security/TokenUtility.java
+++ src/main/java/com/gcep/security/TokenUtility.java
@@ -2,12 +2,16 @@ package com.gcep.security;
 
 import java.util.Date;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class TokenUtility {
@@ -15,25 +19,28 @@ public class TokenUtility {
 	// duration is 72 hours
 	private static final long EXPIRES = 259200000;
 	
-	// make this read from app.properties instead of hard-coded
-	private String secret = "testingTestingTESTING1234567890testingTestingTESTING1234567890";
+	@Value("${app.listeverywhere.secret}")
+	private String secret;
 	
 	public String generateToken(User user) {
 		
+		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
 		
-		
-		// update this function to not use deprecated function
 		return Jwts.builder().setSubject(String.format("%s,%s", user.getUsername(), user.getPassword()))
 				.setIssuer("gcep")
 				.setIssuedAt(new Date())
 				.setExpiration(new Date(System.currentTimeMillis() + EXPIRES))
-				.signWith(SignatureAlgorithm.HS256, secret)
+				.signWith(key)
 				.compact();
+				
 	}
 	
 	public boolean validateToken(String tkn) {
+		
+		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+		
 		try {
-			Jwts.parser().setSigningKey(secret).parseClaimsJws(tkn);
+			Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(tkn);
 			return true;
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -43,7 +50,10 @@ public class TokenUtility {
 	}
 	
 	public String getSubject(String tkn) {
-		Claims c = Jwts.parser().setSigningKey(secret).parseClaimsJws(tkn).getBody();
+		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+		
+		Claims c = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(tkn).getBody();
+		
 		return c.getSubject();
 	}
 
